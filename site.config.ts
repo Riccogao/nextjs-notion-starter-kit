db67bb79853bbcedf7a5c8af567e4d1b27ac89dd
Riccogao <156403578+Riccogao@users.noreply.github.com>
2025-05-28 16:54:00 -0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: '',
   github: 'Riccogao',
-  linkedin: 'www.linkedin.com/in/luna-gao',
+  linkedin: 'luna-gao',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
